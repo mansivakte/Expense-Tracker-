ecaf224dd0bad3dd4f7a54f879203a7d5fb58044
mansivakte <mansi25vv@gmail.com>
2022-12-01 12:26:00 +0530

    chartbar added

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const DUMMY_EXPENSES = [
     amount: 949.21,
     date: new Date(2020, 7, 14),
   },
-  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 6, 12) },
   {
     id: "e3",
     title: "Car Insurance",
